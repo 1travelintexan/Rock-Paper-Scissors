0a7d4fd93494296b4ad830ffeda18f608106eae0
Joshua George <horheyinc8@gmail.com>
2021-12-31 16:29:29 +0000

    matrix theame added

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ let rockBtn = document.querySelector("#rock");
 let paperBtn = document.querySelector("#paper");
 let scissorsBtn = document.querySelector("#scissors");
 let whoWon = document.querySelector("#who_won");
+let gameoverScreen = document.querySelector("#gameover");
+let gameScreen = document.querySelector("#game");
+let userBox = document.querySelector("#user_choice");
+let computerBox = document.querySelector("#computer_choice");
 
 const computerPlay = () => {
   let index = Math.floor(Math.random() * arr.length);
@@ -18,40 +22,65 @@ const playRound = (userChoice, computerChoice) => {
     score1++;
     playerScore.innerHTML = score1.toString();
     whoWon.innerHTML = "You win! Rock smashes Scissors!";
+    userBox.innerHTML = "&#9994";
+    computerBox.innerHTML = "&#9996";
     return "You win, rock smashes scissors!";
   }
   if (userChoice == "Rock" && computerChoice == "Paper") {
     score2++;
     computerScore.innerHTML = score2.toString();
     whoWon.innerHTML = "You lose! Paper covers Rock!";
+    userBox.innerHTML = "&#9994";
+    computerBox.innerHTML = "&#9995";
     return "You lose! Paper covers Rock!";
   }
   if (userChoice == "Scissors" && computerChoice == "Paper") {
     score1++;
     playerScore.innerHTML = score1.toString();
     whoWon.innerHTML = "You win! Scissors cut paper!";
+    userBox.innerHTML = "&#9996";
+    computerBox.innerHTML = "&#9995";
     return "You win! Scissors cuts Paper!";
   }
   if (userChoice == "Scissors" && computerChoice == "Rock") {
     score2++;
     computerScore.innerHTML = score2.toString();
     whoWon.innerHTML = "You lose! Rock smashes scissors!";
+    userBox.innerHTML = "&#9996";
+    computerBox.innerHTML = "&#9994";
     return "You lose! Rock smashes Scissors!";
   }
   if (userChoice == "Paper" && computerChoice == "Rock") {
     score1++;
     playerScore.innerHTML = score1.toString();
     whoWon.innerHTML = "You win! Paper covers rock!";
+    userBox.innerHTML = "&#9995";
+    computerBox.innerHTML = "&#9994";
     return "You win! Paper covers Rock!";
   }
   if (userChoice == "Paper" && computerChoice == "Scissors") {
     score2++;
     computerScore.innerHTML = score2.toString();
     whoWon.innerHTML = "You lose! Scissors cuts paper!";
+    userBox.innerHTML = "&#9995";
+    computerBox.innerHTML = "&#9996";
     return "You lose! Scissors cuts Paper!";
+  } else {
+    if (userChoice == "Paper") {
+      userBox.innerHTML = "&#9995";
+      computerBox.innerHTML = "&#9995";
+    }
+    if (userChoice == "Rock") {
+      userBox.innerHTML = "&#9994";
+      computerBox.innerHTML = "&#9994";
+    }
+    if (userChoice == "Scissors") {
+      userBox.innerHTML = "&#9996";
+      computerBox.innerHTML = "&#9996";
+    }
+    whoWon.innerHTML = "It's a tie! Go again!";
+    return `It's a tie! ${userChoice} is the same a ${computerChoice}`;
   }
-  whoWon.innerHTML = "It's a tie! Go again!";
-  return `It's a tie! ${userChoice} is the same a ${computerChoice}`;
 };
 
 const game = (playerChoice) => {
